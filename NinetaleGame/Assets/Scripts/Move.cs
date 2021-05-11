9cd0204f1b4afb1e810e36bd3a0d7fe6f9a6977a
LeeSoonSin <67868140+LeeSoonSin@users.noreply.github.com>
2021-05-12 00:32:39 +0900

    Jump

--- NinetaleGame/Assets/Scripts/Move.cs
+++ NinetaleGame/Assets/Scripts/Move.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     Rigidbody2D playerRigidbody;
+    public float jumpPower;
     public float maxSpeed;
     SpriteRenderer spriteRenderer;
+    Animator anim;
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
     //Stop Speed
     void Update()
     {
+        if(Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            playerRigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
         if(Input.GetButtonUp("Horizontal"))
         {
             
@@ -26,7 +34,15 @@ public class Move : MonoBehaviour
 
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
-
+        //Animation
+        if(Mathf.Abs(playerRigidbody.velocity.x) < 0.3)
+        {
+            anim.SetBool("isWalking", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", true);
+        }
     }
     void FixedUpdate()
     {
@@ -42,6 +58,22 @@ public class Move : MonoBehaviour
         if (playerRigidbody.velocity.x < (-1) * maxSpeed)
         {
             playerRigidbody.velocity = new Vector2((-1) * maxSpeed, playerRigidbody.velocity.y);
+        }
+        //Landing Platform
+        if(playerRigidbody.velocity.y < 0)
+        {
+            Debug.DrawRay(playerRigidbody.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(playerRigidbody.position, Vector3.down, 1, LayerMask.GetMask("Platform"));//1은 레이의 길이임
+
+            if (rayHit.collider != null) // 빔을 맞지 않았다면? 점프했을때.
+            {
+                if (rayHit.distance < 05f)
+                {
+                    anim.SetBool("isJumping", false);
+                }
+
+            }
         }
     }
 }
--- NinetaleGame/Assets/Scripts/PlayerStat.cs
+++ NinetaleGame/Assets/Scripts/PlayerStat.cs
@@ -31,6 +31,6 @@ public class PlayerStat : MonoBehaviour
 
     private void Die()
     {
-        
+        Debug.Log("¿∏æ”¡Í±›");
     }
 }
