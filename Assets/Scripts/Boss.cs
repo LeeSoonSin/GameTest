6d9778845c5808b206ab5f7b978d32ecf28af4cf
LeeSoonSin <67868140+LeeSoonSin@users.noreply.github.com>
2021-06-10 08:00:45 +0900

    Ending

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boss : Enemy
 {
@@ -273,6 +274,7 @@ public class Boss : Enemy
             color.a -= Time.deltaTime;
             yield return null;
         }
+        SceneManager.LoadScene("EndStory");
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/EndTypingeffect.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndTypingeffect : MonoBehaviour
+{
+    public Text[] tx;
+    public GameObject grid;
+    public Image image;
+    private string[] m_text =
+    {
+        "요괴를 처치하고 산은 다시 생기를 찾았다. \n",
+        "나무들은 모두 원래대로 돌아오고 \n",
+        "밝은 아침햇살이 내려왔다.\n",
+        "        -The End- \n"
+    };
+
+    void Start()
+    {
+        StartCoroutine(_image());
+        StartCoroutine(_typing());
+        grid = GameObject.Find("Grid2");
+        image = GameObject.Find("bg_n (2)").GetComponent<Image>();
+    }
+
+    IEnumerator _typing()
+    {
+        
+        yield return new WaitForSeconds(1.5f);
+        for (int i = 0; i < tx.Length; i++)
+        {
+            for (int j = 0; j < m_text[i].Length; j++)
+            {
+                tx[i].text = m_text[i].Substring(0, j);
+                yield return new WaitForSeconds(0.1f);
+                
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+        yield return new WaitForSeconds(1f);
+    }
+    IEnumerator _image()
+    {
+        Color color = image.color;
+        yield return new WaitForSeconds(1.5f);
+        for (float f = 1.0f; f >= 0.0f; f -= 0.0001f)
+        {
+            color.a = f;
+            image.color = color;
+        }
+    }
+}
